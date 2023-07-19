5d742f35bca4520ecc9d9a702ec50760a7c76064
rakeshast <rakesh.bokde@arsenaltech.com>
2023-07-19 23:15:16 +0530

    Laravel-inertia-vue

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>About Us</h1>
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+</template>
+<script>
+import { Link } from '@inertiajs/vue3';
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Hello World</h1>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</template>
+<script>
+import { Link } from '@inertiajs/vue3';
+</script>
